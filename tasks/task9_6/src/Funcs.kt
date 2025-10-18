632917b071e58c85a7b35555e31ba2f281e09283
stockyy <fnnb5132@leeds.ac.uk>
2025-10-18 15:36:52 +0100

    exceptions covered

--- tasks/task9_6/src/Funcs.kt
+++ tasks/task9_6/src/Funcs.kt
@@ -1,5 +1,18 @@
 // Task 9.6: functions for dataset variance calculation
+import java.io.File
+
 
 // Add variance() function here
+fun variance(dataset: List<Double>): Double {
+    require(dataset.count() > 1) {"Dataset must contain at least 2 values"}
+    val mean = dataset.average()
+    val sumSquaredDeviations = dataset.map{it - mean}.sumOf{it * it}
+    return sumSquaredDeviations / (dataset.size - 1)
+}
 
 // Add readData() function here
+fun readData(filename: String) = buildList {
+    File(filename).forEachLine {
+        add(it.toDouble())
+    }
+}
--- tasks/task9_6/src/Main.kt
+++ tasks/task9_6/src/Main.kt
@@ -4,4 +4,15 @@ import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     // Implement main program here
+    val result = runCatching {
+        val dataset = readData(args[0])
+        val variance = variance(dataset)
+        val datasetSize = dataset.size
+        println("Size: $datasetSize")
+        println("Variance %.5f".format(variance))
+    }
+    if (result.isFailure) {
+        println("Task Failed: ${result.exceptionOrNull()}")
+        exitProcess(1)
+    }
 }
